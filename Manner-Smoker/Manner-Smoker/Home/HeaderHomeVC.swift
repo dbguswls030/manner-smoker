2eb750a8f0ebec86b998660e58fc26b2173d6a2c
MelonMania <dnwlsskfn@gmail.com>
2022-05-23 19:41:01 +0900

    드가자

---
+++ Manner-Smoker/Manner-Smoker/Home/HeaderHomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderHomeVC.swift
+//  Manner-Smoker
+//
+//  Created by RooZin on 2022/05/20.
+//
+
+import UIKit
+
+class HeaderHomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ Manner-Smoker/Manner-Smoker/MyPage/HeaderMyPageTableCell.swift
@@ -0,0 +1,25 @@
+//
+//  HeaderMyPageTableCell.swift
+//  Manner-Smoker
+//
+//  Created by RooZin on 2022/05/19.
+//
+
+import UIKit
+
+class HeaderMyPageTableCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
